e8af3f424c418745d75a930edc8bc1f2dde57a0d
Senthil Rajendran <senthilar@gmail.com>
2020-04-06 06:51:40 -0700

    COde clean up

--- src/main/java/com/home/test/persist/redis/RedisPublisher.java
+++ src/main/java/com/home/test/persist/redis/RedisPublisher.java
@@ -8,8 +8,8 @@ public class RedisPublisher {
     public static void main(String[] args) {
 
         //Connecting to Redis server on localhost
-        Jedis jedis = new Jedis("172.17.0.9", 6379);
-//        JedisCluster jedis = new JedisCluster(new HostAndPort(AppConfig.getClusterIp(), 6379));
+//        Jedis jedis = new Jedis("172.17.0.9", 6379);
+        JedisCluster jedis = new JedisCluster(new HostAndPort(AppConfig.getClusterIp(), 6379));
         System.out.println("Connection to server sucessfully");
 
         jedis.publish("channel", "test message");
--- src/main/java/com/home/test/persist/redis/RedisSubscriber.java
+++ src/main/java/com/home/test/persist/redis/RedisSubscriber.java
@@ -9,14 +9,14 @@ public class RedisSubscriber {
     public static void main(String[] args) {
 
         //Connecting to Redis server on localhost
-        Jedis jedis = new Jedis("172.17.0.9", 6379);
-//        JedisCluster jedis = new JedisCluster(new HostAndPort(AppConfig.getClusterIp(), 6379));
+//        Jedis jedis = new Jedis("172.17.0.9", 6379);
+        JedisCluster jedis = new JedisCluster(new HostAndPort(AppConfig.getClusterIp(), 6379));
         System.out.println("Connection to server sucessfully");
 
         jedis.subscribe(new JedisPubSub() {
             @Override
             public void onMessage(String channel, String message) {
-                // handle message
+                System.out.println("Channel: " + channel + ", Message Received: " + message);
             }
         }, "channel");
     }
